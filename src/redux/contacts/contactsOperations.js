dcfe40d5be78c0ed72d33c198ecac6fc1ba47ff9
Alexandr Neveroff <neveroff1976@gmail.com>
2023-06-30 14:47:33 +0300

    update contactsOperations

--- src/redux/contacts/contactsOperations.js
+++ src/redux/contacts/contactsOperations.js
@@ -2,21 +2,10 @@ import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { Notify } from 'notiflix';
 
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
-
-const authToken = {
-  set(token) {
-    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-  },
-};
-
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
   async (_, thunkAPI) => {
     try {
-      const state = thunkAPI.getState();
-      const persistedToken = state.auth.token;
-      authToken.set(persistedToken);
       const { data } = await axios.get('/contacts');
       return data;
     } catch (error) {
@@ -29,9 +18,6 @@ export const addContact = createAsyncThunk(
   'contacts/addContact',
   async (newContact, thunkAPI) => {
     try {
-      const state = thunkAPI.getState();
-      const persistedToken = state.auth.token;
-      authToken.set(persistedToken);
       const { data } = await axios.post('/contacts', newContact);
       Notify.success(`${newContact.name} added to phonebook successfully!`);
       return data;
@@ -45,9 +31,6 @@ export const deleteContact = createAsyncThunk(
   'contacts/deleteContact',
   async (contactId, thunkAPI) => {
     try {
-      const state = thunkAPI.getState();
-      const persistedToken = state.auth.token;
-      authToken.set(persistedToken);
       const { data } = await axios.delete(`/contacts/${contactId}`);
       Notify.warning(`${data.name} delete from phonebook.`);
       return data.id;
@@ -61,9 +44,6 @@ export const editContact = createAsyncThunk(
   'contacts/editContact',
   async ({ contactId, name, number }, thunkAPI) => {
     try {
-      const state = thunkAPI.getState();
-      const persistedToken = state.auth.token;
-      authToken.set(persistedToken);
       const { data } = await axios.patch(`/contacts/${contactId}`, {
         name,
         number,
